0f31e6b9f31a4cd06dfe57e1e03c54033ca01376
BlackBerry009 <765042894@qq.com>
2023-03-19 11:48:18 +0800

    refactor cli

--- src/init.ts
+++ src/init.ts
@@ -1,7 +1,18 @@
+import path from 'node:path';
 import * as p from '@clack/prompts';
 import fse from 'fs-extra';
 import logSymbols from 'log-symbols';
 import color from 'picocolors';
+import { fileURLToPath } from 'node:url';
+
+const getDirName = (filename: string) => {
+  const templateDir = path.resolve(
+    fileURLToPath(import.meta.url),
+    '../..',
+    `template-${filename}`,
+  );
+  return templateDir;
+};
 
 export const main = async () => {
   const project = await p.group(
@@ -16,14 +27,6 @@ export const main = async () => {
           options: [
             { value: 'react', label: 'React' },
             { value: 'vue', label: 'Vue' },
-          ],
-        }),
-      variant: () =>
-        p.select({
-          message: 'Which types you wanna choose?',
-          options: [
-            { value: 'ts', label: 'ts' },
-            { value: 'js', label: 'js' },
           ],
         }),
     },
@@ -42,6 +45,36 @@ export const main = async () => {
       );
       return;
     }
+    const s = p.spinner();
+    s.start('Creating project...');
+    const templateDir = getDirName('react-ts');
+    fse.cp(
+      templateDir,
+      `./${project.name}`,
+      {
+        recursive: true,
+      },
+      (err) => {
+        if (err) {
+          s.stop(`Creating project failed: ${err}`);
+          return;
+        }
+        s.stop(`Creating project success!`);
+        let nextSteps = `cd ${project.name} \n
+        pnpm install \n
+        pnpm start`;
+
+        p.note(nextSteps, 'Next steps.');
+
+        p.outro(
+          `Problems? ${color.underline(
+            color.cyan(
+              'https://github.com/BlackBerry009/create-boilerplate-cli/issues',
+            ),
+          )}`,
+        );
+      },
+    );
   }
   if (project.framework === 'vue') {
     console.log(
